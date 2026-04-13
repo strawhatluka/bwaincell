43c2ed7871a7ec42e96d2b2958fdec050c13111a
Luka Fagundes <luka@sunny-stack.com>
2026-04-13 15:07:23 -0700

    refactor(supabase): Remove all models and client index

--- supabase/types.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/**
- * Supabase Database Types
- * Matches existing PostgreSQL schema from Sequelize models.
- */
-Object.defineProperty(exports, '__esModule', { value: true });
-//# sourceMappingURL=types.js.map
